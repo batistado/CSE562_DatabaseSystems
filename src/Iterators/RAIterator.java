c63312d77c2f0de549ea00ef6293578c8ec36fda
batistado <syed.kamran@live.com>
2019-02-21 21:46:23 -0500

    Modified path

--- src/Iterators/RAIterator.java
+++ src/Iterators/RAIterator.java
@@ -11,7 +11,7 @@ import net.sf.jsqlparser.statement.select.SelectItem;
 public interface RAIterator extends Iterator<ArrayList<PrimitiveValue>>  {
 	//public final String DIR = "/Users/msyed3/Downloads/sample queries/Sanity_Check_Examples/data/";
 	//public final String DIR = "/Users/msyed3/Downloads/sample queries/NBA_Examples/";
-	public final String DIR = "/data/";
+	public final String DIR = "data/";
 	
 	public void resetWhere();
 	
