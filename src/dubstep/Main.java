728d9a6d7d3d9687e3058fcf8c2e31794193a07b
batistado <syed.kamran@live.com>
2019-03-25 14:18:18 -0400

    Corrected CLI arguments

--- src/dubstep/Main.java
+++ src/dubstep/Main.java
@@ -48,11 +48,13 @@ public class Main {
 	
 	
 	public static void main(String[] args) throws FileNotFoundException, UnsupportedEncodingException {
-		if (args.length > 0 && args[0].equals("--in-mem")){
-            isInMemory = true;
-        } else {
-        	isInMemory = false;
-        }
+		isInMemory = false;
+		
+		for (String arg : args) {
+			if (arg.equals("--in-mem")){
+	            isInMemory = true;
+	        }
+		}
 		
 		
 		CCJSqlParser parser;
