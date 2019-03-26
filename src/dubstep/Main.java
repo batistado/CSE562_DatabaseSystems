11cabab08f0db94c5ae078f649c2c4812ab8db42
batistado <syed.kamran@live.com>
2019-03-26 15:33:46 -0400

    Reduced the no of sorted runs to 2

--- src/dubstep/Main.java
+++ src/dubstep/Main.java
@@ -45,7 +45,7 @@ import net.sf.jsqlparser.statement.select.Union;
 public class Main {
 	public static Map<String, TupleSchema> tableSchemas = new HashMap<>();
 	public static boolean isInMemory;
-	public static Integer sortedRunSize = 10;
+	public static Integer sortedRunSize = 2;
 	
 	
 	public static void main(String[] args) throws FileNotFoundException, UnsupportedEncodingException {
